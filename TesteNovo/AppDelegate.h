b42c1e80bbb69b80101e4cc14d4c2ecca0645a98
mathemf <apple@Matheus.local>
2014-03-19 15:17:52 -0300

    Initial Commit

---
+++ TesteNovo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TesteNovo
+//
+//  Created by Matheus on 19/03/14.
+//  Copyright (c) 2014 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TesteNovo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TesteNovo
+//
+//  Created by Matheus on 19/03/14.
+//  Copyright (c) 2014 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
